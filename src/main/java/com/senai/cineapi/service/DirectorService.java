e80858e5e62fc68ae8e3a50df06a08a84aeb514e
Sheldron <sheldrongrandini@gmail.com>
2026-07-10 10:25:16 -0300

    Creation of service package and DirectorService class

---
+++ src/main/java/com/senai/cineapi/service/DirectorService.java
@@ -0,0 +1,44 @@
+package com.senai.cineapi.service;
+
+import com.senai.cineapi.dto.DirectorRequest;
+import com.senai.cineapi.model.Director;
+import com.senai.cineapi.repository.DirectorRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class DirectorService {
+
+    private final DirectorRepository directorRepository;
+
+    public DirectorService(DirectorRepository directorRepository) {
+        this.directorRepository = directorRepository;
+    }
+
+    //Cria um objeto diretor e faz validação de campos
+    public Director create(DirectorRequest request) {
+        if (request.getName() == null || request.getName().trim().isEmpty()) {
+            throw new IllegalArgumentException("O nome do diretor é obrigatório e não pode conter apenas espaços.");
+        }
+
+        if (request.getNationality() == null || request.getNationality().trim().isEmpty()) {
+            throw new IllegalArgumentException("A nacionalidade do diretor é obrigatória e não pode conter apenar espaços.");
+        }
+
+        Director director = new Director();
+        director.setName(request.getName().trim());
+        director.setNationality(request.getNationality().trim());
+
+        return directorRepository.save(director);
+    }
+
+    //Pega todos os objetos diretor
+    public List<Director> findAll() {
+        return directorRepository.findAll();
+    }
+
+    //Retorna um objeto diretor de acordo com o id
+    public Director findById(Long id){
+        return directorRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Diretor de ID " + id + " não foi encontrado."));
+    }
+}
---
+++ src/main/java/com/senai/cineapi/service/MovieService.java
@@ -0,0 +1,4 @@
+package com.senai.cineapi.service;
+
+public class MovieService {
+}
