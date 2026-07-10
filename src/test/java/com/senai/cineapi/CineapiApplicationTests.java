2b6af8beef92af721dcaaeccea005f51f95d85ed
Sheldron <sheldrongrandini@gmail.com>
2026-07-10 09:00:03 -0300

    Innitial commit CineAPI

---
+++ src/test/java/com/senai/cineapi/CineapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.cineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
